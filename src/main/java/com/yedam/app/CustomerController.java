385bffbd1a990e843a7209bf88e653c239dfd0a8
GlRlBOY <wkd3795@hanmail.net>
2023-11-09 10:37:59 +0900

    controller 수정

--- src/main/java/com/yedam/app/CustomerController.java
+++ src/main/java/com/yedam/app/CustomerController.java
@@ -3,16 +3,26 @@ package com.yedam.app;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.yedam.app.accessingdatajpa.Customer;
 import com.yedam.app.accessingdatajpa.CustomerRepository;
 
-@RestController  //컨트롤러, 리스폰스바디
+@RestController // 컨트롤러, 리스폰스바디
 public class CustomerController {
-	@Autowired CustomerRepository repo;
+	@Autowired
+	CustomerRepository repo;
+
+	@GetMapping("/")
+	public ModelAndView main() {
+		ModelAndView mv = new ModelAndView("home");
+		mv.addObject("greet", "<b>하이</b>");
+		return mv;
+	}
+	
 	@GetMapping("/customer")
-	public Iterable<Customer> findall(){
+	public Iterable<Customer> findall() {
 		return repo.findAll();
-		
+
 	}
 }
